64e7d4eadf29c06dbe3ab077c80b254a3aeacb1e
Meet Shihora <142139362+meetshihora@users.noreply.github.com>
2024-08-09 11:29:06 +0530

    Add files via upload

---
+++ Day17/Task1.py
@@ -0,0 +1,5 @@
+# Task 1: create any set anf try to use frozenset(setname)
+
+set_A = {1, 2, 3, 4, 5}
+frozen_set_A = frozenset(set_A)
+print(frozen_set_A)
---
+++ Day17/Task2.py
@@ -0,0 +1,13 @@
+''' Task 2: Find the elements in a given set that are not in another set
+
+        set1 = {1,2,3,4,5}
+        set2 = {4,5,6,7,8}
+
+        diffrence between set1 and set2 is {1,2,3}
+'''
+
+
+set1 = {1,2,3,4,5}
+set2 = {4,5,6,7,8}
+
+print(set1 - set2)
